6a8dbf8c3464e6e0b8e382e7463104c7df9d2f91
nahom temesgen nadew <nadewnahom8@gmail.com>
2025-10-30 20:20:59 +0300

    add code for binary selectio sort

--- dsa/sorting/selection_sort.cpp
+++ dsa/sorting/selection_sort.cpp
@@ -5,13 +5,11 @@ void selectionSort(int arr[], int n) {
     for (int i = 0; i < n - 1; i++) {
         int minIndex = i;
 
-        // Find the minimum element in the unsorted part
         for (int j = i + 1; j < n; j++) {
             if (arr[j] < arr[minIndex])
                 minIndex = j;
         }
 
-        // Swap the found minimum element with the first unsorted element
         swap(arr[i], arr[minIndex]);
     }
 }
