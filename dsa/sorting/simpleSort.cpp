2b1deb0cf681e754c14ec3ff3e7fe59e88a19f37
chapi1420 <nadewnahom8@gmail.com>
2025-10-31 08:36:39 +0300

    another update

---
+++ dsa/sorting/simpleSort.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+void simpleSort(int arr[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                swap(arr[j], arr[j + 1]);
+            }
+        }
+    }
+}
+
+int main() {
+    int arr[] = {5, 3, 8, 4, 2};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    simpleSort(arr, n);
+
+    cout << "Sorted array: ";
+    for (int i = 0; i < n; i++)
+        cout << arr[i] << " ";
+    cout << endl;
+
+    return 0;
+}
