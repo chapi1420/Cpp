073fc834337aebf9b7e98cbe9c09de7d5095c5db
chapi1420 <nadewnahom8@gmail.com>
2025-11-07 09:46:06 +0300

    completee deletion yet display is remaining

--- dsa/linkedlist/doubly_linked_list/full_doubly.cpp
+++ dsa/linkedlist/doubly_linked_list/full_doubly.cpp
@@ -42,28 +42,67 @@ void insertEnd(Node*& head, int num)
 
 void insertMid(Node*& head, int position, int num)
 {
+    if(position <= 1){
+        insertBeginning(head, num);
+    }
 Node* newNode =new Node();
 newNode->data =num;
+Node* temp = head;
+for (int i = 1; temp != nullptr && i < position - 1; i++) {
+    temp = temp->next;
+}
 
+if (temp == nullptr) {
+    cout << "Position out of range!" << endl;
+    delete newNode;
+    return;
+    }
+temp->next->prev =newNode;
+newNode->next = temp->next;
+newNode->prev =temp;
+temp->next = newNode;
 
 }
 
 
 void deleteBeg(Node*& head)
 {
-
+if(head ==nullptr){
+    cout<<"the linked lisst is empty, nothing to delete"<<endl;
+    return;
+    }
+Node* temp =head;
+head =head->next;
+delete temp;
+head->prev =nullptr;
 }
 
 
 void deleteEnd(Node*& head)
-{
+{ if(head==nullptr){
+    cout<<"nothing to delete, list is already empty"<<endl;
+}
+if (head->next == nullptr) {
+        delete head;
+        head = nullptr;
+        return;
+    }
+
+    Node* temp =head;
+    while (temp->next->next != nullptr)
+        temp = temp->next;
+
+    temp->next->prev =nullptr;
+    delete temp->next;
+    temp->next = nullptr;
+
 
 }
 
 
 void deleteMid(Node*& head, int position)
 {
-
+Node* 
 }
 
 
