e9e9938c2f532d488ff5e460d9f039ac40d497a8
chapi1420 <nadewnahom8@gmail.com>
2025-11-28 07:29:42 +0300

    application on queue, priority based on gender implementation

---
+++ dsa/queue/priorities/genderPriority.cpp
@@ -0,0 +1,58 @@
+#include<iostream>
+using namespace std;
+
+
+
+struct Person {
+    string gender;
+    Person* next;
+};
+
+struct GenderPQ {
+    Person* front;
+    Person* rear;
+};
+
+void init(GenderPQ &q) {
+    q.front = q.rear = NULL;
+}
+
+void enqueue(GenderPQ &q, string gender) {
+    Person* p = new Person();
+    p->gender = gender;
+    p->next = NULL;
+
+    if (gender == "female") {
+        p->next = q.front;
+        q.front = p;
+        if (q.rear == NULL) q.rear = p;
+    }
+    else { // male
+        if (q.rear == NULL) q.front = q.rear = p;
+        else {
+            q.rear->next = p;
+            q.rear = p;
+        }
+    }
+}
+
+string dequeue(GenderPQ &q) {
+    if (q.front == NULL) return "empty";
+
+    Person* temp = q.front;
+    string val = temp->gender;
+    q.front = q.front->next;
+    if (q.front == NULL) q.rear = NULL;
+
+    delete temp;
+    return val;
+}
+
+void display(GenderPQ &q) {
+    Person* cur = q.front;
+    while (cur != NULL) {
+        cout << cur->gender << " ";
+        cur = cur->next;
+    }
+    cout << endl;
+}
