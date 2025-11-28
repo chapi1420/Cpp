79d5e4b2b3d8d31746e805c9c0f91c79b62dfb4c
chapi1420 <nadewnahom8@gmail.com>
2025-11-28 08:11:13 +0300

    add main function

--- dsa/stack/ArrayBased.cpp
+++ dsa/stack/ArrayBased.cpp
@@ -37,3 +37,28 @@ int peek() {
     }
     return stackArr[top];
 }
+
+
+int main() {
+    // Push elements
+    push(10);
+    push(20);
+    push(30);
+
+    // Display top element
+    cout << "Top element: " << peek() << endl;
+
+    // Pop an element
+    cout << "Popped: " << pop() << endl;
+
+    // Push another
+    push(40);
+
+    // Pop all to show stack behavior
+    cout << "Popped: " << pop() << endl;
+    cout << "Popped: " << pop() << endl;
+    cout << "Popped: " << pop() << endl;   // this will trigger underflow
+
+    return 0;
+}
+
