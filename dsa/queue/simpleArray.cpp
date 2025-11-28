96dd8025cf7c0515d9a93ddbb0753af61a048e43
chapi1420 <nadewnahom8@gmail.com>
2025-11-28 07:15:49 +0300

    compolete simple queue

---
+++ dsa/queue/simpleArray.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+using namespace std;
+
+struct Queue {
+    int arr[100];
+    int front;
+    int rear;
+};
+
+void init(Queue &q) {
+    q.front = 0;
+    q.rear = -1;
+}
+
+bool isFull(Queue &q) {
+    return q.rear == 99;
+}
+
+bool isEmpty(Queue &q) {
+    return q.front > q.rear;
+}
+
+void enqueue(Queue &q, int val) {
+    if (isFull(q)) {
+        cout << "Queue Overflow\n";
+        return;
+    }
+    q.arr[++q.rear] = val;
+}
+
+int dequeue(Queue &q) {
+    if (isEmpty(q)) {
+        cout << "Queue Underflow\n";
+        return -1;
+    }
+    return q.arr[q.front++];
+}
+
+int peek(Queue &q) {
+    if (isEmpty(q)) return -1;
+    return q.arr[q.front];
+}
+
+void display(Queue &q) {
+    if (isEmpty(q)) {
+        cout << "Queue empty\n";
+        return;
+    }
+    for (int i = q.front; i <= q.rear; i++)
+        cout << q.arr[i] << " ";
+    cout << endl;
+}
